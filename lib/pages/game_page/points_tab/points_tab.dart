3bda44eafb37b8942bb6b7eec2290214591d0f1a
MichelHanzenScheeren <michelhanzenscheeren@gmail.com>
2020-05-18 15:35:01 -0300

    mudança dos arquivos da tabPoints para uma pasta interna de widgets, para melhor organização.

--- lib/pages/game_page/points_tab/points_tab.dart
+++ lib/pages/game_page/points_tab/points_tab.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './player.dart';
-import './truco_button.dart';
-import './undo_round_button.dart';
+import './widgets/player.dart';
+import './widgets/truco_button.dart';
+import './widgets/undo_round_button.dart';
 
 class PointsTab extends StatelessWidget {
   @override
